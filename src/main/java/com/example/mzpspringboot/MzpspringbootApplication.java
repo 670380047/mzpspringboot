807aa3c6db62b615658eeaaea343db45e64e159b
mzp <670380047@qq.com>
2019-04-12 17:36:46 +0800

    springboot打war包。 启动项和pom配置

--- src/main/java/com/example/mzpspringboot/MzpspringbootApplication.java
+++ src/main/java/com/example/mzpspringboot/MzpspringbootApplication.java
@@ -6,15 +6,18 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MzpspringbootApplication extends SpringBootServletInitializer {
+public class MzpspringbootApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args)  {
 		SpringApplication.run(MzpspringbootApplication.class, args);
 		System.out.println("项目启动成功。。。。。");
 		}
 
-//	启动类继承了SpringBootServletInitializer就可以正常部署在外部tomcat中了，主要起到web.xml的作用
-    protected SpringApplicationBuilder config(SpringApplicationBuilder builder){
-	    return builder.sources(MzpspringbootApplication.class);
-    }
+
+	//	启动类继承了SpringBootServletInitializer就可以正常部署在外部tomcat中了，主要起到web.xml的作用
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		//这个参数是启动类。和main方法的第一个参数一样
+		return builder.sources(MzpspringbootApplication.class);
+	}
 }
