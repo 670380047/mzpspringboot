8b892ae0136dd78d793bec0a54402f56721d7f23
mzp <670380047@qq.com>
2019-04-20 09:23:46 +0800

    springboot

---
+++ src/main/java/com/example/mzpspringboot/model/Book.java
@@ -0,0 +1,78 @@
+package com.example.mzpspringboot.model;/**
+ * @version: java version 1.7+
+ * @Author : mzp
+ * @Time : 2019/4/17 11:22
+ * @File : Book
+ * @Software: IntelliJ IDEA 2019.3.15
+ */
+
+/**
+ *
+ * @Author maozp3
+ * @Description:
+ * @Date: 2019/4/17 11:22
+ */
+/*//jpa实体
+@Entity
+@Table(name = "book")
+public class Book {
+    //@Id表明这是一个主键。
+    // @GeneratedValue是主键生成的策略。AUTO： JPA自动选择合适的策略，是默认选项。IDENTITY：采用数据库ID自增长的方式来自增主键字段
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String reader;
+    private String isbn;
+    private String title;
+    private String author;
+    private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReader() {
+        return reader;
+    }
+
+    public void setReader(String reader) {
+        this.reader = reader;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}*/
