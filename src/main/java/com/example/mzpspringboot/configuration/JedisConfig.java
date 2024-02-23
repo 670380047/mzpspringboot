101aa6ee4593811c76f9ea83dbd7b1d990abbf3f
mzp <670380047@qq.com>
2024-02-23 15:22:31 +0800

    Bean的命名规则

--- src/main/java/com/example/mzpspringboot/configuration/JedisConfig.java
+++ src/main/java/com/example/mzpspringboot/configuration/JedisConfig.java
@@ -1,6 +1,7 @@
 package com.example.mzpspringboot.configuration;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.JedisSentinelPool;
 
@@ -17,8 +18,21 @@ import java.util.Set;
 @Configuration
 @Slf4j
 public class JedisConfig {
+    /**
+     *  @Bean注入的类的命名规则：
+     *          1.可以使用name属性或者value来指定bean的名字
+     *          2. 如果不配置的话，默认使用方法名作为bean的名字
+     *
+     * @Controller、 @Service 、@Component 、@Repository这协助解来注入Bean时，Bean的命名规则:
+     *          1.可以使用value属性来指定名称
+     *          2.如果不配置的话，对于以驼峰命名的类来说（比如JedisConfig.class），bean的名字就是类名首字母小写：jedisConfig
+     *              但是，对于不是驼峰命名的类来说，比如（MZPController.class），当类的前两个字母都是大写的时候，
+     *              就直接用这个类名来作为Bean的名称（原封不动）：MZPController
+     *              其逻辑在：org.springframework.context.annotation.AnnotationBeanNameGenerator#generateBeanName()这个方法的最后一层的decapitalize()方法中
+     * @return
+     */
     //因为在测试lettuce客户端。暂时先不需要注入到容器中
-//    @Bean
+    @Bean
     public JedisSentinelPool createJedisPool(){
         //主节点的名称。 要和 sentinel.conf中配置的名称一样
         String masterName = "mzp-redis-master";
